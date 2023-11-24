36ea19ff18db111fbc14914dd68a16080ce88678
Igor Kantar <igor_kan_bl@hotmail.com>
2023-11-24 14:15:38 +0100

    add: widgets

--- window.py
+++ window.py
@@ -1,7 +1,14 @@
+from PySide6.QtCore import Qt
+
 from PySide6.QtWidgets import (
     QWidget,
     QVBoxLayout,
+    QLabel,
+    QPushButton,
+    QSplitter,
 )
+
+from button_list import ButtonList
 
 class MainWidget(QWidget):
 
@@ -9,5 +16,18 @@ class MainWidget(QWidget):
         super().__init__()
         self.parent = parent
         self.layout = QVBoxLayout()
+        self.label  = QLabel("Sites:")
+        self.buttonList = ButtonList(self)
+        self.addButton  = QPushButton("+")
+
+        self.layout.setAlignment(Qt.AlignTop)
+
+        for widget in [
+            self.label,
+            self.buttonList,
+            QSplitter(),
+            self.addButton,
+        ]:
+            self.layout.addWidget(widget)
 
         self.setLayout(self.layout)
